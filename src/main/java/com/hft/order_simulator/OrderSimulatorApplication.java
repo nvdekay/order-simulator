40212ef971e09dad0752cd116faead90f302c6bd
nvdekay <khanhnvd.work@gmail.com>
2025-08-01 15:57:33 +0700

    first commit

---
+++ src/main/java/com/hft/order_simulator/OrderSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.hft.order_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hft/order_simulator/OrderSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hft.order_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
